8465310cd7e67a6a1a53a97fb7425ba62995e764
KirHanMell666 <kuba.meller@gmail.com>
2025-02-23 22:10:48 +0100

    Prepare environment

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,18 @@
+using System;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    private float _baseSpeed;
+    private Rigidbody2D _rb;
+    
+    private void Start()
+    {
+        
+    }
+
+    private void Update()
+    {
+        throw new NotImplementedException();
+    }
+}
