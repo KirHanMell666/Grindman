6a0480ffd3821870de0c7a3e57331b731d528a6e
KirHanMell666 <kuba.meller@gmail.com>
2025-02-26 21:24:18 +0100

    simple movement

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -3,16 +3,56 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    private float _baseSpeed;
+    [SerializeField] private float baseSpeed;
+    [SerializeField] private float jump;
     private Rigidbody2D _rb;
+    
+    private float moveVelocity;
+
+    //Grounded Vars
+    bool isGrounded = true;
     
     private void Start()
     {
-        
+        _rb = GetComponent<Rigidbody2D>();
     }
 
-    private void Update()
+    private void Update () 
     {
-        throw new NotImplementedException();
+        //Jumping
+        if (Input.GetKeyDown(KeyCode.Space) || Input.GetKeyDown(KeyCode.UpArrow)) 
+        {
+            if(isGrounded)
+            {
+                _rb.linearVelocity = new Vector2 (_rb.linearVelocity.x, jump);
+                isGrounded = false;
+            }
+        }
+
+        moveVelocity = 0;
+
+        //Left Right Movement
+        if (Input.GetKey(KeyCode.LeftArrow) || Input.GetKey(KeyCode.A)) 
+        {
+            moveVelocity = -GetSpeed();
+        }
+        if (Input.GetKey(KeyCode.RightArrow) || Input.GetKey(KeyCode.D)) 
+        {
+            moveVelocity = GetSpeed();
+        }
+
+        _rb.linearVelocity = new Vector2 (moveVelocity, _rb.linearVelocity.y);
+
+    }
+    
+    //Check if Grounded
+    private void OnTriggerEnter2D()
+    {
+        isGrounded = true;
+    }
+
+    private float GetSpeed()
+    {
+        return baseSpeed;
     }
 }
